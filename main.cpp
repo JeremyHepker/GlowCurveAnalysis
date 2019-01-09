36b529b0f089bdb907a5a895909373c9b58ddbbb
jhepker <jhepker@umich.edu>
2019-01-09 17:12:07 -0500

    Initil Commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  GlowCurveAnalsys
+//
+//  Created by jeremy hepker on 1/9/19.
+//  Copyright © 2019 Jeremy Hepker. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+    
+}
